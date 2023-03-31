0e3fdc1c910d700bcbafe56ae4e2a075dc98d024
moezdurrani <103555283+moezdurrani@users.noreply.github.com>
2023-03-30 21:57:33 -0500

    Make the search Bar Responsive

--- script.js
+++ script.js
@@ -28,22 +28,37 @@ else{
     localStorage.setItem("theme", "light");
 }
 
-/**Trying out things now */
 
-var story1 = document.getElementById("storyy1");
-var storyy2 = document.getElementById("storyy2");
+let video = 0;
+videoholder.style.transform = 'scaleY(0%)'
+function videoOpen(){
+    
+    if(video == 0){
+        videoholder.style.transform = 'scaleY(100%)'
+        video = 1;
+    }
+    else if(video == 1){
+        videoholder.style.transform = 'scaleY(0%)'
+        video = 0;
+    }
+}
 
-// story1.addEventListener("onmouseleave",shrink1());
-// story1.addEventListener("onmouseenter",scale1());
+let ind = 0;
+websiteView.style.transform = 'scaleY(0%)'
+function searchBing(){
 
-// document.querySelector("storyy2").addEventListener("mouseenter", function () {
-//     alert("Ouch! Stop poking me!");
-//   });
-
-// const myImage = document.querySelector("storyy1");
-// myImage.mouseenter = scale1();
-// myImage.mouseleave = shrink1();
-
+    var txt = document.getElementById('userSearch').value;
+    document.getElementById('web').src = 'https://www.google.com/search?igu=1&q=' + txt;
+    
+    if(ind == 0){
+        websiteView.style.transform = 'scaleY(100%)'
+        ind = 1;
+    }
+    else if(ind == 1){
+        websiteView.style.transform = 'scaleY(0%)'
+        ind = 0;
+    }
+}
 
 //Scaling up the stories when hovering the mouse over them------------------------
 function scale1()
@@ -87,81 +102,9 @@ function shrink5()
     storyy5.style.transform = 'scale(1)'
 }
 
-//changing like and unlike icon
 
-// var firstLike = document.querySelector("#like_unlike1");
-// firstLike.addEventListener("click", changeIconn, false);
-
-// function changeIconn(e){
-//     //let displayImage = e.target.id;
-//     let displayImage = e.target.document.getElementById('like_unlike1');
-//     if(displayImage.src.match('images/like-red.png')){
-//         displayImage.src = 'images/like.png'
-//     } 
-//     else {
-//         displayImage.src = 'images/like-red.png'
-//     }
-// }
-
-
-function changeIcon(){
-    let displayImage = document.getElementById('like_unlike1')
-    if(displayImage.src.match('images/like-red.png')){
-        displayImage.src = 'images/like.png'
-    } 
-    else {
-        displayImage.src = 'images/like-red.png'
-    }
-}
-function changeIcon2(){
-    let displayImage = document.getElementById('like_unlike2')
-    if(displayImage.src.match('images/like-red.png')){
-        displayImage.setAttribute("src", 'images/like.png');
-        //displayImage.src = 'images/like.png'
-    } 
-    else {
-        //displayImage.src = 'images/like-red.png'
-        displayImage.setAttribute("src", 'images/like-red.png');
-    }
-}
-function changeIcon3(){
-    let displayImage = document.getElementById('like_unlike3')
-    if(displayImage.src.match('images/like-red.png')){
-        displayImage.src = 'images/like.png'
-    } 
-    else {
-        displayImage.src = 'images/like-red.png'
-    }
-}
-function changeIcon4(){
-    let displayImage = document.getElementById('like_unlike4')
-    if(displayImage.src.match('images/like-red.png')){
-        displayImage.src = 'images/like.png'
-    } 
-    else {
-        displayImage.src = 'images/like-red.png'
-    }
-}
-function changeIcon5(){
-    let displayImage = document.getElementById('like_unlike5')
-    if(displayImage.src.match('images/like-red.png')){
-        displayImage.src = 'images/like.png'
-    } 
-    else {
-        displayImage.src = 'images/like-red.png'
-    }
-}
-function changeIcon6(){
-    let displayImage = document.getElementById('like_unlike6')
-    if(displayImage.src.match('images/like-red.png')){
-        displayImage.src = 'images/like.png'
-    } 
-    else {
-        displayImage.src = 'images/like-red.png'
-    }
-}
-function changeIcon7(){
-    let displayImage = document.getElementById('like_unlike7')
+function changeIcon(id){
+    let displayImage = document.getElementById(id)
     if(displayImage.src.match('images/like-red.png')){
         displayImage.src = 'images/like.png'
     } 
@@ -170,3 +113,88 @@ function changeIcon7(){
     }
 }
 
+let myNumber = 50;
+let myNumber2 = 87;
+let myNumber3 = 34;
+let myNumber4 = 12;
+let myNumber5 = 97;
+let myNumber6 = 50;
+let myNumber7 = 97;
+
+let flip = 0 //unlike position
+document.getElementById("numlik1").textContent = myNumber;
+document.getElementById("numlik2").textContent = myNumber2;
+document.getElementById("numlik3").textContent = myNumber3;
+document.getElementById("numlik4").textContent = myNumber4;
+document.getElementById("numlik5").textContent = myNumber5;
+document.getElementById("numlik6").textContent = myNumber6;
+document.getElementById("numlik7").textContent = myNumber7;
+
+function Incr(id){
+    if (flip == 0)
+    {
+        flip = 1;
+
+        if(id == 'numlik1'){
+            myNumber += 1;
+            document.getElementById(id).textContent = myNumber;
+        }
+        else if(id == 'numlik2'){
+            myNumber2 += 1;
+            document.getElementById(id).textContent = myNumber2;
+        }
+        else if(id == 'numlik3'){
+            myNumber3 += 1;
+            document.getElementById(id).textContent = myNumber3;
+        }
+        else if(id == 'numlik4'){
+            myNumber4 += 1;
+            document.getElementById(id).textContent = myNumber4;
+        }
+        else if(id == 'numlik5'){
+            myNumber5 += 1;
+            document.getElementById(id).textContent = myNumber5;
+        }
+        else if(id == 'numlik6'){
+            myNumber6 += 1;
+            document.getElementById(id).textContent = myNumber6;
+        }
+        else if(id == 'numlik7'){
+            myNumber7 += 1;
+            document.getElementById(id).textContent = myNumber7;
+        }
+    }
+    else
+    {
+        flip = 0;
+
+        if(id == 'numlik1'){
+            myNumber -= 1;
+            document.getElementById(id).textContent = myNumber;
+        }
+        else if(id == 'numlik2'){
+            myNumber2 -= 1;
+            document.getElementById(id).textContent = myNumber2;
+        }
+        else if(id == 'numlik3'){
+            myNumber3 -= 1;
+            document.getElementById(id).textContent = myNumber3;
+        }
+        else if(id == 'numlik4'){
+            myNumber4 -= 1;
+            document.getElementById(id).textContent = myNumber4;
+        }
+        else if(id == 'numlik5'){
+            myNumber5 -= 1;
+            document.getElementById(id).textContent = myNumber5;
+        }
+        else if(id == 'numlik6'){
+            myNumber6 -= 1;
+            document.getElementById(id).textContent = myNumber6;
+        }
+        else if(id == 'numlik7'){
+            myNumber7 += 1;
+            document.getElementById(id).textContent = myNumber7;
+        }
+    }
+}
